ad3f09f906ae96bf4655be8ae431eee2cb8dd721
yevhen-prokopets <akkermanzena99@gmail.com>
2021-07-26 13:15:18 +0300

    Initial commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,25 @@
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class FirstTest {
+    @Test
+    public void firstTest(){
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.warmdevs.com/");
+        driver.findElement(By.linkText("Start a project")).click();
+        driver.findElement(By.xpath("//*[@id=\"email-form\"]/div[2]/div[2]/label/input")).click();
+        driver.findElement(By.xpath("//*[@id=\"NAME-2\"]")).sendKeys("Sans");
+        driver.findElement(By.xpath("//*[@id=\"PHONE\"]")).sendKeys("380990117237");
+        driver.findElement(By.xpath("//*[@id=\"Email\"]")).sendKeys("eugene@warmdevs.com");
+        driver.findElement(By.xpath("//*[@id=\"COMPANY-2\"]")).sendKeys("Starship");
+        driver.findElement(By.xpath("//*[@id=\"PROJECT\"]")).sendKeys("Brand new ship");
+        driver.findElement(By.xpath("//*[@id=\"email-form\"]/input")).click();
+
+    }
+}
